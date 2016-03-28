702d15a5e111d40e37776b28d782435d13b19f35
Hanno Rein <hanno@hanno-rein.de>
2016-03-28 16:30:33 -0400

    minor

--- webcam/make_hour.bash
+++ webcam/make_hour.bash
@@ -1,13 +1,17 @@
 #!/bin/bash
-hourlist=""
-hour800list=""
-for i in `seq 1 1 60`;
-do
-    ni=`expr 1440 - ${i}`
-    pni=`printf '%04d' ${ni}`
-    hourlist="frames/loop${pni}.gif $hourlist"
-    hour800list="frames800/loop${pni}.gif $hour800list"
-done
+#hourlist=""
+#hour800list=""
+#for i in `seq 1 1 60`;
+#do
+#    ni=`expr 1440 - ${i}`
+#    pni=`printf '%04d' ${ni}`
+#    hourlist="frames/loop${pni}.gif $hourlist"
+#    hour800list="frames800/loop${pni}.gif $hour800list"
+#done
+#
+#convert  -delay 4  -loop 0 ${hourlist} loop_hour.gif
+#convert  -delay 4  -loop 0 ${hour800list} loop800_hour.gif
 
-convert  -delay 4  -loop 0 ${hourlist} loop_hour.gif
-convert  -delay 4  -loop 0 ${hour800list} loop800_hour.gif
+LID=`cat lid.txt`
+LIDHOUR=$((LID-60))
+avconv -r 20 -start_number $LIDHOUR -i frames_id/frame_%09d.jpg -c:v libx264 -y last_hour.mp4
--- webcam/uploadVideoToTwitter.bash
+++ webcam/uploadVideoToTwitter.bash
@@ -2,16 +2,16 @@
 START="$1"
 MESSAGE="$2"
 
-avconv -r 40 -start_number $1 -i frames800/loop%04d.gif -c:v libx264 -y out.mp4
-if [ `stat --format=%Y out.mp4` -ge $(( `date +%s` - 6000 )) ]; then
-    SIZE=`stat --format="%s" out.mp4`
+avconv -r 40 -start_number $1 -i frames_id/frame_%09d.jpg -c:v libx264 -y last_day.mp4
+if [ `stat --format=%Y last_day.mp4` -ge $(( `date +%s` - 6000 )) ]; then
+    SIZE=`stat --format="%s" last_day.mp4`
     echo "$SIZE"
     MEDIAJSON=`/usr/local/bin/twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=INIT&media_type=video/mp4&total_bytes=$SIZE"`
     MEDIAID=`echo "$MEDIAJSON" | sed -rn 's/\{\"media_id\":(.*)\,\"media_id_string.*/\1/p'`
     echo "$MEDIAJSON"
     echo "$MEDIAID"
     INDEX=0
-    split -b 5m out.mp4 twitter-video-
+    split -b 5m last_day.mp4 twitter-video-
     for FILE in twitter-video-*; do
         echo "[INFO] Uploading segment $INDEX ($FILE)..."
         /usr/local/bin/twurl "/1.1/media/upload.json" -H upload.twitter.com -d "command=APPEND&segment_index=$INDEX&media_id=$MEDIAID" --file-field "media" --file "$FILE"
